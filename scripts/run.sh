7b126c28324632d8f9b0c2f9786f88ff02c89184
BenHesketh21 <heskethben0@gmail.com>
2020-10-07 16:56:17 +0100

    Changed port

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo docker run -d -p 5000:5000 --name challenge-3 localhost:8082/challenge-3:latest
+sudo docker run -d -p 5001:5001 --name challenge-3 localhost:8082/challenge-3:latest
