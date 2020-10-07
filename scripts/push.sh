b41c72f0f255fc477ab381c61243670f12a46d75
BenHesketh21 <heskethben0@gmail.com>
2020-10-07 16:52:06 +0100

    Added latest push

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
+
 sudo docker push localhost:8082/challenge-3:${BUILD_NUMBER}
+sudo docker tag localhost:8082/challenge-3:${BUILD_NUMBER} localhost:8082/challenge-3:latest
+sudo docker push localhost:8082/challenge-3:latest
